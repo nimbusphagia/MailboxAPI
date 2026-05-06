a34c7d01df82ca3ea805e1230488fb06d280828d
Ignacio Machuca <185011694+nimbusphagia@users.noreply.github.com>
2026-05-05 19:53:52 -0500

    fix cookie cors config

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,23 +1,26 @@
 import "dotenv/config";
 import cookieParser from "cookie-parser";
-import express from 'express'
-import cors from 'cors'
-import { errorMiddleware } from "./errorHandler"
-import indexRouter from "../routes/index.router"
-import { authMiddleware } from "../middleware/auth.middleware"
-import authRouter from "../routes/auth.router"
+import express from "express";
+import cors from "cors";
+import { errorMiddleware } from "./errorHandler";
+import indexRouter from "../routes/index.router";
+import { authMiddleware } from "../middleware/auth.middleware";
+import authRouter from "../routes/auth.router";
 
-const app = express()
+const app = express();
 
 app.use(cookieParser());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 // Public
 app.use("/auth", authRouter);
